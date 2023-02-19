a3fb49c69ffc77c79202670175c630541e3f1862
Kevin Tuyizere <tuyizerek27@gmail.com>
2023-02-19 20:26:17 +0200

    init

--- config/db_config.js
+++ config/db_config.js
@@ -1,19 +1,21 @@
-const mysql = require("mysql");
-const dotenv = require("dotenv").config();
+const mysql = require('mysql');
+const dotenv = require('dotenv').config();
+
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
 });
 
 connection.connect((err) => {
-  if (err) {
-    throw err.message;
-  } else {
-    console.log("Database connection established");
-  }
+    if (err) {
+        throw err.message;
+    }else{    
+        console.log('Database connection established');
+    }
 });
 
 module.exports = connection;
